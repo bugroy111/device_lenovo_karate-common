c138efd1dee8212f8d2b67577746ffae79a4b0b9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-05-07 07:49:15 +0800

    karate: Set USB MIDI IDs
    For now, we set it as the default IDs provided.
    
    Also remove the obsolete charging ID.
    
    Change-Id: Ie42f1ed0e19757e20f70756c5e94613daadb01fc

--- product/usb.mk
+++ product/usb.mk
@@ -1,3 +1,8 @@
+# MIDI
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
