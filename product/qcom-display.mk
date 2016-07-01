bb3050975a0e30c4647eca9a921587dbd6a6cca0
Ananta Kishore K <akollipa@codeaurora.org>
2016-07-01 12:33:30 +0530

    karate: Add vulkan features
    Vulkan hardware level & API version features
    are required to enable Vulkan Android CTS tests
    execution.
    
    Change-Id: Id75826c8f1d9528f8e40fe24109b5a9ded30043b

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -6,3 +6,8 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8937 \
     liboverlay
+
+# Vulkan
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
