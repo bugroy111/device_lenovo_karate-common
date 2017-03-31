57f6093b9f136ccbd61230d3f008c2851bfd5dd3
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    karate-common: Add libbt-vendor in device-common.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -1,3 +1,7 @@
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
