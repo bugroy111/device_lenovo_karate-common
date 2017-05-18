85fc196c9ceab7d34e5b9e401d0e53c762d81783
c_rrajiv <rrajiv@codeaurora.org>
2017-05-18 18:54:35 +0530

    karate-common: Include wificond and wifilogd in the build package
    WiFi needs wificond and wifilogd service to be part of the build
    package.
    
    CRs-Fixed: 2051165
    Change-Id: Ica8b592caa561270f9feb909be0654a32c44148a

--- product/wifi.mk
+++ product/wifi.mk
@@ -1,3 +1,8 @@
+# Wifi
+PRODUCT_PACKAGES += \
+    wificond \
+    wifilogd
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
