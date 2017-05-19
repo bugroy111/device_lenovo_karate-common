1650e098d7c99e4713bc4cab8b100d665948e10a
karthick111 <karthickrko61@gmail.com>
2017-05-19 19:56:35 +0200

    karate: Add kernel tasks config
    Change-Id: I05ad5f35b90698dfccc9ad4163c64737c586768c

---
+++ board/kernel.mk
@@ -0,0 +1,11 @@
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.console=tty60 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 gpt androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_SOURCE := kernel/lenovo/msm8937
+TARGET_KERNEL_CONFIG := karate_defconfig
