91ed74a472072dafdb8619292a38514a91320bfd
karthick111 <karthickrko61@gmail.com>
2017-05-19 20:23:54 +0530

    karate: Initial camera configs
    Change-Id: I4fd5681fd604a0a0b7eab2a54a2e9c89f80682ed

---
+++ product/qcom-camera.mk
@@ -0,0 +1,11 @@
+# Snap
+PRODUCT_PACKAGES += \
+    Snap \
+    libbson
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/imx219_karateof_chromatix.xml:system/etc/camera/imx219_karateof_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/imx219_karateqt_chromatix.xml:system/etc/camera/imx219_karateqt_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/imx258_karateof_chromatix.xml:system/etc/camera/imx258_karateof_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/imx258_karateqt_chromatix.xml:system/etc/camera/imx258_karateqt_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/lenovo_karate_camera.xml:system/etc/camera/lenovo_karate_camera.xml
