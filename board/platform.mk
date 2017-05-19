3a40a2befb024815d4db60ccbbe492f77ed125e2
karthick111 <karthickrko61@gmail.com>
2017-05-19 16:31:45 +0200

    karate: Enable Qualcomm support
    * Add BOARD_USES_QCOM_HARDWARE
    * Add BOARD_USES_QC_TIME_SERVICES
    * Build platform specific PowerHAL
    * Build CAF RIL
    
    Change-Id: I0f35932345216c319bba60832e538d503b571e0f

--- board/platform.mk
+++ board/platform.mk
@@ -7,3 +7,9 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QC_TIME_SERVICES := true
+TARGET_POWERHAL_VARIANT := qcom
+TARGET_RIL_VARIANT := caf
