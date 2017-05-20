8463a2ca5bce48484211c53757c28f7c8a778b93
karthick111 <karthickrko61@gmail.com>
2017-05-20 22:48:18 +0530

    karate: Import system.prop from msm8937_32
    Change-Id: I8f17fe2ebd40652081cbd135a6a6123811560e28

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -20,8 +20,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles_8956.xml:system/etc/media_profiles_8956.xml
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=1 \
-    vidc.enc.narrow.searchrange=1
