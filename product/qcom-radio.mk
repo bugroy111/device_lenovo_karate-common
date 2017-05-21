f81d1d8e3a514a238254b5d15493529f9f0bc2d8
karthick111 <karthickrko61@gmail.com>
2017-05-21 20:35:38 +0530

    karate: Add IRSC config
    Change-Id: Ibcc788daa321a6a79ed8c32608d5d2395d2aeb9d

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -3,6 +3,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+
 # QMI
 PRODUCT_PACKAGES += \
     dsi_config.xml \
