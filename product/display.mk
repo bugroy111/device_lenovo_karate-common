3d689409a52c738c2068ee3d4cb14e28e0567a6b
karthick111 <karthickrko61@gmail.com>
2017-05-21 21:05:14 +0530

    karate: Add display configs
    Change-Id: Ie180aecca1398153e96332b4772dfdb9150e223b

---
+++ product/display.mk
@@ -0,0 +1,14 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
+
+# Dalvik
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
---
+++ product/qcom-display.mk
@@ -0,0 +1,8 @@
+# Display
+PRODUCT_PACKAGES += \
+    copybit.msm8937 \
+    gralloc.msm8937 \
+    hwcomposer.msm8937 \
+    libtinyxml \
+    memtrack.msm8937 \
+    liboverlay
