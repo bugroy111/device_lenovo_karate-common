eff3fd05e28595d62b6a57db16f66450b45028be
karthick111 <karthickrko61@gmail.com>
2017-06-05 17:23:41 +0200

    karate: Cleanup wifi configs
    Change-Id: I7d810561b9dbaf8d53e441987808b64bad593de6

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -8,9 +8,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libwpa_client \
     libqsap_sdk \
-    libQWiFiSoftApCfg \
-
-# WCNSS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-    $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
+    libQWiFiSoftApCfg
