189f97c51752d4d4fb85e5b35c9de6d5273eceff
karthick111 <karthickrko61@gmail.com>
2017-06-08 22:18:16 +0530

    karate: Support other variants
    Change-Id: I87991d59319d58be21b27f2a2dc034779a27b4c6

--- init/Android.mk
+++ init/Android.mk
@@ -19,6 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := system/core/init
+LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_MODULE := libinit_karate
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_karate.cpp
