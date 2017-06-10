8044a0ce7c7898a49f0777c2f2e194b34ddbae9e
karthick111 <karthickrko61@gmail.com>
2017-06-10 10:35:29 +0530

    karate: Add missing blobs
    * Also build sensors from source
    
    Change-Id: I4b4fd04d4a0779834483d7c8037d071e35e9b3b0

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,4 +1,7 @@
 # Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8937
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
