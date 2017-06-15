38a880327ac303f8200c866b47046ee5890c7a7b
karthick111 <karthickrko61@gmail.com>
2017-06-15 06:14:16 +0200

    karate: Add missing audio_platform_info_extcodec.xml
    Change-Id: I9507013392eb36bf08a3ee6b047dc9c787d57978

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -2,6 +2,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths_karate_a42.xml:system/etc/mixer_paths_karate_a42.xml \
     $(LOCAL_PATH)/audio/mixer_paths_karate_a48.xml:system/etc/mixer_paths_karate_a48.xml \
