1b52b02af3b81dcae17762252b75839f3c8d3fb8
karthick111 <karthickrko61@gmail.com>
2017-06-22 21:58:26 +0530

    karate: Update proprietary-files.txt

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,3 +1,7 @@
+# Idc
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinputtv.idc:system/usr/idc/uinputtv.idc
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
