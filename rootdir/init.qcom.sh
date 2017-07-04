c02e6e541b66fb0c0775905e972e17ead60ae4f8
karthick111 <karthickrko61@gmail.com>
2017-07-04 18:43:44 +0200

    karate: Cleanup rootdir

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -29,22 +29,6 @@
 chown -h system:system /sys/devices/soc/qpnp-smbcharger-*/battery_max_current
 target=`getprop ro.board.platform`
 
-start_sensors()
-{
-    if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
-        chmod -h 775 /persist/sensors
-        chmod -h 664 /persist/sensors/sensors_settings
-        chown -h system.root /persist/sensors/sensors_settings
-
-        mkdir -p /data/misc/sensors
-        chmod -h 775 /data/misc/sensors
-
-        start sensors
-    fi
-}
-
-
-start_sensors
 start msm_irqbalance
 
 #
