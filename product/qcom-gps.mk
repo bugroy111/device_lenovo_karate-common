92272579d5deb63819dc373d09ff459b776327ed
karthick111 <karthickrko61@gmail.com>
2017-08-19 16:50:21 +0200

    karate: Update gps stack from addison

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,9 +1,16 @@
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8937 \
-    gps.conf \
     libcurl \
     libgnsspps
+
+PRODUCT_PACKAGES += \
+    flp.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
 
 PRODUCT_BOOT_JARS += \
     com.qti.location.sdk
