604f460470389a477528a06c2bd528a12c0bd7b0
karthick111 <karthickrko61@gmail.com>
2017-08-28 18:05:45 +0530

    karate: Initial arm64 bringup.

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,16 +1,15 @@
-# Kernel
+# CPUsets
+ENABLE_CPUSETS := true
+
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.console=tty60 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 gpt androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
-
-ENABLE_CPUSETS := true
 
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8937
 TARGET_KERNEL_CONFIG := karate_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+
 TARGET_USE_SDCLANG := true
