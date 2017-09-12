272147d1405bca00896788016fdcaf1e136484d7
karthick111 <karthickrko61@gmail.com>
2017-09-13 01:05:58 +0800

    Revert "karate: Remove QTI Telephony for single sim variants"
    This reverts commit 6f617c6abac5a44ca48536f240344d6d6cefcdc1.

--- configs/check_features.sh
+++
@@ -1,9 +0,0 @@
-#!/sbin/sh
-
-lenovo_id="/sys/devices/soc0/platform_lenovo_hardware_type"
-
-if [ "$lenovo_id" = "S82937EA1" ] || [ "$lenovo_id" = "S82937CA1" ]; then
-    rm -rf /system/app/QtiTelephonyService
-    rm /system/framework/QtiTelephonyServicelibrary.jar
-    rm /system/framework/qti-telephony-common.jar
-fi
