0c89969be16f9d399cd37642c6fadd16f87d4471
Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>
2017-09-19 21:34:25 +0530

    karate-common: Add android.hardware.drm@1.0-service.widevine
    Add android.hardware.drm@1.0-service.widevine to PRODUCT_PACKAGES.
    android.hardware.drm@1.0-service.widevine is needed for
    WV13 complete use case.
    
    Change-Id: Id5d6a53bb5d5a8bc677b783b584454b782a42289

--- product/drm.mk
+++ product/drm.mk
@@ -1,4 +1,5 @@
 # DRM Impl HAL
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine
