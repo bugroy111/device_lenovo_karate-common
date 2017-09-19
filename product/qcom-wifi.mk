1e655868a5d4b40e15bd493d8eda29d2c26d15b4
karthick111 <karthickrko61@gmail.com>
2017-09-19 20:59:04 +0200

    karate: Build missing packages

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -1,10 +1,14 @@
 # Wifi
 PRODUCT_PACKAGES += \
+    ipacm \
+    ipacm-diag \
+    IPACM_cfg.xml \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
+    libwpa_client \
     libqsap_sdk \
     libQWiFiSoftApCfg
 
