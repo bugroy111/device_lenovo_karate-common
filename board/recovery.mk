06c9fc351241e588b7f93a4d5b5bd8e5a8c978a3
karthick111 <karthickrko61@gmail.com>
2017-10-10 11:42:21 +0200

    karate: Move rootdir scripts to rootdir/etc
    * also build init instead of copying

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,5 +1,5 @@
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 PRODUCT_PACKAGES += \
     librecovery_updater_qcom
