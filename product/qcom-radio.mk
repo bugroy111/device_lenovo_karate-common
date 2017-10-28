45db57b4c7880af09af3fc16994aa485f33accef
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-27 19:21:28 -0700

    karate-common: Add powerhint list and QTI broadcast whitelist
    Change-Id: I07e01be15ed0964834ff16dbebbbc84f9ceefcf2

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -15,3 +15,7 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl \
     libxml2
+
+# Qualcomm broadcast whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
