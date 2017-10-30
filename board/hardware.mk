a618bf8a0014fefd6093f4cf0a26aecac0f4c160
karthick111 <karthickrko61@gmail.com>
2017-10-30 13:10:11 +0100

    karate-common: enable dt2w

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,3 +1,6 @@
 # CMHW
 BOARD_HARDWARE_CLASS := hardware/cyanogen/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
+
+# Tap-to-Wake
+TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input1/device/wake_gesture"
