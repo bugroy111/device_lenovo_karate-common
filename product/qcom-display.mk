2e838854ba11c355b3cb38dff2e500506917e387
karthick111 <karthickrko61@gmail.com>
2017-12-10 15:58:00 +0100

    karate-common: Cleanup configurations

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -7,8 +7,3 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     memtrack.msm8937
-
-# Vulkan
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -14,7 +14,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_BOOT_JARS += \
     com.qti.location.sdk
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
