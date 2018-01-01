8728321e75a75db087ea25cb7018db7d00ec4618
karthick111 <karthickrko61@gmail.com>
2018-01-01 07:23:16 +0100

    karate-common: use lineagehw

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,7 +1,7 @@
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
+    hardware/lineage/lineagehw
 
 # Tap-to-Wake
 TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input1/device/wake_gesture"
