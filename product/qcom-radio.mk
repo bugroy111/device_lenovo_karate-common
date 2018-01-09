ff0855cf33a9aebc2498e15a97ec303f6ca6c00f
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    karate-common: Build RCS and telephony-ext
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -24,3 +24,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
