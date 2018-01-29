97b72345f035e15cd68a810c53f26c197f8691e7
Ravi Kishore R <ravikish@codeaurora.org>
2018-01-29 11:23:33 +0530

    karate-common: RILD: Remove copying of QCRIL DB from vendor partition
    Remove this redundant mechanism of copying DB.
    QCRIL Daemon already takes care of the copying
    at runtime
    
    Change-Id: I2213c2a8a1c9717c92f3cf5e7873c0ee9e65da35

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -28,18 +28,7 @@
 
 chown -h system:system /sys/devices/soc/qpnp-smbcharger-*/battery_max_current
 
-start_copying_prebuilt_qcril_db()
-{
-    if [ -f /system/vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
-        cp /system/vendor/qcril.db /data/misc/radio/qcril.db
-        chown -h radio.radio /data/misc/radio/qcril.db
-    fi
-}
-
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
-
-start_copying_prebuilt_qcril_db
-echo 1 > /data/misc/radio/db_check_done
 
 #
 # Make modem config folder and copy firmware config to that folder for RIL
