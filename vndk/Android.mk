18b8a173f46f031b7dc869627972f6636ea36127
Justin Yun <justinyun@google.com>
2018-02-01 16:40:28 +0900

    karate-common: vndk: Add missing vndk-sp libs to the VNDK-SP list
    For the Treble devices without BOARD_VNDK_VERSION, we must have a
    list of vndk-sp libs.
    If there are some missing vndk-sp libs in the list, add them to the
    list automatically.
    
    Bug: 72470805
    Bug: 69526027
    Test: build sailfish/marlin and check boot
    Change-Id: Ifc30ae32b6353141954bdcb8b0e40940382eb8a4
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Signed-off-by: K-Teja <tejachaitu96@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,6 +1,3 @@
-# TODO(b/69526027): DEPRECATE USE OF THIS.
-# USE BOARD_VNDK_VERSION:=current instead.
-
 LOCAL_PATH := $(call my-dir)
 
 # b/69526027: This VNDK-SP install routine must be removed. Instead, we must
@@ -13,7 +10,7 @@ ifndef BOARD_VNDK_VERSION
 # However, some of those libs need FWK-ONLY libs, which must be listed here
 # manually.
 VNDK_SP_LIBRARIES := \
-    libdexfile_support \
+    libdexfile
 
 install_in_hw_dir := \
    android.hidl.memory@1.0-impl
@@ -31,7 +28,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
-LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -46,7 +42,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
-LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
@@ -56,9 +51,6 @@ endef
 $(foreach lib,$(VNDK_SAMEPROCESS_LIBRARIES),\
     $(if $(filter $(lib),$(VNDK_SP_LIBRARIES)),,\
     $(eval VNDK_SP_LIBRARIES += $(lib))))
-
-# Remove libz from the VNDK-SP list (b/73296261)
-VNDK_SP_LIBRARIES := $(filter-out libz,$(VNDK_SP_LIBRARIES))
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
     $(eval $(call define-vndk-sp-lib,$(lib))))
@@ -71,4 +63,7 @@ LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
+
+install_in_hw_dir :=
+vndk_sp_dir :=
 endif
