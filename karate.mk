3e7cf5e1bb345459ae0b1acaecf3f0f180bd2595
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    karate-common: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- karate.mk
+++ karate.mk
@@ -19,6 +19,7 @@ LOCAL_PATH := device/lenovo/karate-common
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Include device-specific product fragments
 include $(LOCAL_PATH)/product/*.mk
