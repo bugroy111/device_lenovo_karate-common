3e7146fed25631b71095f34083fee13ad6813404
teja98 <tejachaitu96@gmail.com>
2018-02-27 17:12:25 +0100

    karate-common: update ramdisk
    * Bringback init.target.rc
    * Cleanup usb script
    * Merge lenovo specific changes in init.lenovo.common.rc
    
    Signed-off-by: teja98 <tejachaitu96@gmail.com>

--- product/init.mk
+++ product/init.mk
@@ -1,10 +1,12 @@
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.lenovo.common.rc \
     init.qcom.bt.sh \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.target.rc \
     ueventd.qcom.rc
