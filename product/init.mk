cb76a38d7e9bf48c7ac5f9341a670affc66c2930
karthick111 <karthickrko61@gmail.com>
2018-04-21 06:08:56 +0200

    Revert "karate-common: Restore qcom post_boot script"
    This reverts commit b885721431575b2dd28885515387698cb193acbb.

--- product/init.mk
+++ product/init.mk
@@ -2,7 +2,7 @@
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.lenovo.common.rc \
-    init.qcom.post_boot.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
