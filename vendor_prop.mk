5ac1c2d5d45dde4434aa4bd6c112f968f9301ebd
Nirmal Abraham <nabrah@codeaurora.org>
2018-06-13 16:46:56 +0530

    karate-common: Rename properties as per treble compliance
    CRs-Fixed: 2206282
    Change-Id: Idcd4cec9fdd886388ee539eb1134daada855ad4b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -68,8 +68,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=480 \
-    sdm.debug.disable_skip_validate=1 \
-    vendor.display.enable_default_color_mode=1
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.enable_fb_ubwc=1
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
