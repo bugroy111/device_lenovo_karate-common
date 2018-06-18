93908883d1de66fbfcc417d4ed90962e5da70085
karthick111 <karthickrko61@gmail.com>
2018-06-19 01:20:49 +0530

    karate-common: Decommonize button jack keylayout

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -2,5 +2,4 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fts_ts.kl:system/usr/keylayout/fts_ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
-    $(LOCAL_PATH)/keylayout/msm8937-karate-snd-card_Button_Jack.kl:system/usr/keylayout/msm8937-karate-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
