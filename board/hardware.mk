a173582e0c84d37ce37ce93966ad378be38155b1
karthick111 <karthickrko61@gmail.com>
2018-06-25 03:40:55 +0530

    karate-common: Use procfs node for dt2w

--- board/hardware.mk
+++ board/hardware.mk
@@ -3,4 +3,4 @@ BOARD_HARDWARE_CLASS += \
     device/lenovo/karate-common/lineagehw
 
 # Tap-to-Wake
-TARGET_TAP_TO_WAKE_NODE := "/sys/class/input/input1/device/wake_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
