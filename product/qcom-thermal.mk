9a6df2d304d3d8a5c390373f51c42c466c8a5682
karthick111 <karthickrko61@gmail.com>
2018-08-01 06:47:00 +0200

    karate-common: Remove thermal HAL
    * Also cleanup device manifest

--- product/qcom-thermal.mk
+++ product/qcom-thermal.mk
@@ -1,6 +1,3 @@
 # Thermal
-PRODUCT_PACKAGES += \
-    thermal.msm8937
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
--- product/thermal.mk
+++
@@ -1,4 +0,0 @@
-#Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
