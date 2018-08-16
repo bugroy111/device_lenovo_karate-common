eb15cedbec98efd7b633f765514a5b37b3580a6e
karthick111 <karthickrko61@gmail.com>
2018-08-16 05:19:36 +0200

    karate-common: camera: Unlink libgui from shared libraries

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -105,7 +105,7 @@ LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
-LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync libgui
+LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 ifeq ($(USE_DISPLAY_SERVICE),true)
