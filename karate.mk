297c4fd214f7f37f78b0559e762b83fe970f4296
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    karate-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- karate.mk
+++ karate.mk
@@ -27,6 +27,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS += *
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Lenovo/karatep/K53a48:7.0/NRD90N/K53_S228_171114_ROW:user/release-keys"
 
