9bb913363df443c132e0b3feec3f614d77ae194a
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2018-11-16 13:15:51 +0530

    karate-common: Enabling USB enumeration in power-off charging mode
    sys.usb.configfs can't be set from .sh file as per new Android
    sepolicy rules. Hence set sys.usb.configfs in target rc file based on
    ro.boot.usbconfigfs property for USB to work properly in
    power-off charging mode.
    
    Change-Id: I075c1d33e4018099e3deca64e8b2737f1a0c1d4b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,6 +132,7 @@ DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.console=tty60 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 gpt androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=false
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_USE_XZ := true
