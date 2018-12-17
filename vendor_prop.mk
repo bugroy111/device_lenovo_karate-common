4506b5f941c5061488f5e2272570d608c8804739
Artem Borisov <dedsa2002@gmail.com>
2018-12-17 18:02:34 +0400

    karate-common: Disable debug.sf.latch_unsignaled
    Apparently, it introduces dramatical performance degradation in some use cases.
    
    Change-Id: I92aaa4469f6848e4dfff89f3781f31ed527bae2b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -90,7 +90,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
