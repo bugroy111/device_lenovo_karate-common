5947f83f94edb1ae89b583b4a680833a4614ab28
electimon <electimon@gmail.com>
2018-12-29 03:30:07 +0800

    karate-common: Update perfboostsconfig.xml for new perf stack
     * Restore default values from CAF.
     * Remove app launch (disable packing) as it sets unsupported opcodes
       and is furthermore unused.
     * Remove SCHED_PREFER_IDLE opcode as it is unsupported.
    
    Change-Id: I6373e15a54b4904582c324c79b734d76019f0f95
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- karate.mk
+++ karate.mk
@@ -302,6 +302,10 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libstagefright_soft_flacdec
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
