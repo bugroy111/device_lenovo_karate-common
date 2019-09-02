f4ff9955df7dc13f5ca2b3d9f53e8993a6c147c6
karthick111 <karthickrko61@gmail.com>
2019-09-02 10:41:16 +0530

    karate-common: camera: Suppress unused variable warning
    Change-Id: I91eaa07ec1725f2065ff7a8d98a5a5f43712b249

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -7899,7 +7899,7 @@ camera_metadata_t* QCamera3HardwareInterface::translateCapabilityToMetadata(int 
 
     /*target fps range: use maximum range for picture, and maximum fixed range for video*/
     float max_range = 0.0;
-    float max_fixed_fps = 0.0;
+    float __attribute__ ((unused)) max_fixed_fps = 0.0;
     int32_t fps_range[2] = {0, 0};
     for (uint32_t i = 0; i < gCamCapability[mCameraId]->fps_ranges_tbl_cnt;
             i++) {
